17741fcf1c42a67637faecc73ec89b292577c32a
Italo <contato.italo1020@gmail.com>
2020-01-08 09:52:58 -0300

    validação customizada: email não repetido na inserção de cliente.

--- src/main/java/com/br/curso/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/br/curso/service/validation/ClienteInsertValidator.java
@@ -6,17 +6,28 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.br.curso.domain.Cliente;
 import com.br.curso.domain.enuns.TipoCliente;
 import com.br.curso.dto.ClienteNewDTO;
+import com.br.curso.repository.ClienteRepository;
 import com.br.curso.resource.exceptions.FieldMessage;
 import com.br.curso.service.util.Validator;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
-
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public boolean isValid(ClienteNewDTO obj, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<FieldMessage>();
 
+		Cliente cliBuscado = repository.findByEmail(obj.getEmail());
+		if(cliBuscado != null) {
+			list.add(new FieldMessage("email","O email já existe"));
+		}
+		
 		if (obj.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !Validator.validarCPF(obj.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}

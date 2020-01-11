aed8c0e7dbc039c9fdcaa6d40a1908ac32fee890
Italo <contato.italo1020@gmail.com>
2020-01-11 08:51:13 -0300

    Implementando SmtpEmailService com servidor do Google. Implantado e testado

--- src/main/java/com/br/curso/config/DevConfig.java
+++ src/main/java/com/br/curso/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.br.curso.service.DBservice;
+import com.br.curso.service.EmailService;
+import com.br.curso.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 			return true;
 		}
 
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }

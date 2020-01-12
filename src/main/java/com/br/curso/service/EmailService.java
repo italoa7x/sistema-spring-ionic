c67703bcdd920033742acb46fc0460d211b6fc38
Italo <contato.italo1020@gmail.com>
2020-01-12 10:19:32 -0300

    enviando email formatado com html

--- src/main/java/com/br/curso/service/EmailService.java
+++ src/main/java/com/br/curso/service/EmailService.java
@@ -1,12 +1,18 @@
 package com.br.curso.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.br.curso.domain.Pedido;
 
 public interface EmailService {
- 
+
 	void sendOrderConfirmationEmail(Pedido pedido);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/br/curso/service/MockeEmailService.java
+++ src/main/java/com/br/curso/service/MockeEmailService.java
@@ -1,17 +1,27 @@
 package com.br.curso.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
 public class MockeEmailService extends AbstractEmailService {
 	private static final Logger LOG = LoggerFactory.getLogger(MockeEmailService.class);
-	
+
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email Html");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+
 	}
 
 }
